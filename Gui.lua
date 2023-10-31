9d63454e1112b5fdd6ff32df5bf4fb68c093f582
GodlyDragonXD <148655003+GodlyDragonXD@users.noreply.github.com>
2023-10-31 13:11:01 +0000

    Update Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 local Window = Rayfield:CreateWindow({
     Name = "Galaxy Hub| Flight And more!",
     LoadingTitle = "Galaxy Hub",
@@ -29,7 +29,7 @@ local Window = Rayfield:CreateWindow({
 
  local Section = Tab:CreateSection("Section Example")
 
- local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+ local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
  local Window = Rayfield:CreateWindow({
      Name = "Galaxy Hub| Flight And more!",
      LoadingTitle = "Galaxy Hub",
@@ -49,9 +49,9 @@ local Window = Rayfield:CreateWindow({
     
     
      KeySettings = {
-        Title = "Untitled",
+        Title = "Galaxy Hub",
         Subtitle = "Key System",
-        Note = "No method of obtaining the key is provided",
+        Note = "Yes method. shalt not tell",
         FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
         SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
         GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
